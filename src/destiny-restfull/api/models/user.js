ab1d68a0550c35fc5c1d00b3440c894e9b5c41e3
tangbo04 <tangbo04@58ganji.com>
2017-12-29 10:39:20 +0800

    需求大纲初稿

--- src/destiny-restfull/api/models/user.js
+++ src/destiny-restfull/api/models/user.js
@@ -3,16 +3,47 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 var UserSchema = new Schema({
-  user_name: {
+  //  name: 'john',
+  //  email: 'john@email.com',
+  //  signupToken: 'ef32a95a-d6ee-405a-8e4b-515b235f7c54',
+  //  signupTimestamp: Wed Jan 15 2014 19:08:27 GMT+0100 (CET),
+  //  signupTokenExpires: Wed Jan 15 2014 19:08:27 GMT+0100 (CET),
+  //  failedLoginAttempts: 0,
+  //  salt: '48cf9da376703199c30ba5c274580c98',
+  //  derived_key: '502967e5a6e55091f4c2c80e7989623f051070fd',
+  name: {
     type: String,
-    required: 'Kindly enter the name of table'
+    required: 'Kindly enter the name of user'
   },
-  user_comment: {
+  email: {
+    type: String,
+    required: 'Kindly enter the email of user'
+  },
+  signupToken: {
     type: String
   },
-  created_date: {
+  email: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  signupTimestamp: {
     type: Number,
     default: Date.now()
+  },
+  signupTokenExpires: {
+    type: Number,
+    default: Date.now()
+  },
+  salt: {
+    type: String
+  },
+  derived_key: {
+    type: String
   },
   status: {
     type: String,
--- src/destiny-web/src/views/players/index.vue
+++ src/destiny-web/src/views/players/index.vue
@@ -3,6 +3,7 @@
      角色设定<br/>
      角色性格，三观，情感，命运，服饰，各方面的社会地位，与其它角色的关系等等<br/>
      角色各个时间点的重大事件，出场，退场，剧情关键人，....<br/>
+     可以根据章节查看各个人物的状态<br/>
      <el-button-group>
   <el-button type="primary" icon="el-icon-arrow-left">添加角色设定</el-button>
   <el-button type="primary">添加角色关系<i class="el-icon-arrow-right el-icon--right"></i></el-button>
